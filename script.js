8ccc76a74361304533fcb6174e49f23d27128d79
Adir <42638312+Adir-SL@users.noreply.github.com>
2021-07-08 07:17:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function keyCheck(e) {
     document.getElementsByClassName("absBtn")[0].style.backgroundColor = "#CCE2FF";
     document.getElementsByClassName("absBtn")[0].style.color = "#001D6C";
     document.getElementsByClassName("absBtn")[0].style.display = "block";
-    //Old animations...
+    //Old animations - 6.7.21
     // e.target.style.transform = "translateY(-50%)";
     // setTimeout(function () {
     //     setTimeout(function () {
