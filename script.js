3671ad7260c5c16a55451132958bbaaeb516f803
Adir <42638312+Adir-SL@users.noreply.github.com>
2021-08-08 08:01:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -263,24 +263,25 @@ function toggleLang(e){
 }
 function selectMe(e){
     setTimeout(function(){
-    document.getElementById("textField").classList.remove("fieldSelected");
-    resetButtons();
-    e.target.classList.add("selectButton");
-    if(e.target.parentNode.id == "innerWords"){
-        var x = document.getElementsByTagName("button");
-        var i;
-        for (i = 0; i < x.length; i++) {
-            x[i].classList.remove("lastWord");
-        }
+        document.getElementById("textField").classList.remove("fieldSelected");
+        resetButtons();
+        e.target.classList.add("selectButton");
+        if(e.target.parentNode.id == "innerWords"){
+            var x = document.getElementsByTagName("button");
+            var i;
+            for (i = 0; i < x.length; i++) {
+                x[i].classList.remove("lastWord");
+            }
 
-        tempY = 0;
-        document.getElementById("textField").innerHTML = '';
-        getSelectedWord(e.target.innerText);
-        e.target.classList.add("lastWord");
-    }
-    moreButtons();
-    document.getElementById("textField").classList.remove("fieldSelected");
-}, 10);
+            tempY = 0;
+            document.getElementById("textField").innerHTML = '';
+            getSelectedWord(e.target.innerText);
+            e.target.classList.add("lastWord");
+        }
+        moreButtons();
+        document.getElementById("textField").classList.remove("fieldSelected");
+        // setTimeout(function(){ document.getElementById("textField").classList.remove("fieldSelected"); }, 10);
+    }, 10);
 }
 function getSelectedWord(y){
     if(tempY == y.length){
